0ec5630293a2281473b81cf5b1cef17400886a98
Andersen Newman <andersen.newman@bellevuecollege.edu>
2018-10-29 12:15:20 -0700

    10/29/18

---
+++ JavaScriptTest/week07-day01.js
@@ -0,0 +1,32 @@
+const num = 3;
+const numberString = '3';
+const rain = true;
+
+console.log(num, numberString, rain);
+console.log(typeof num, typeof numberString, typeof rain);
+console.log(num + numberString);
+console.log(typeof num);
+
+if (rain === true) {
+    console.log('Go to the gym.');
+} else {
+    console.log('Go for a walk.');
+}
+
+console.log('MODULUS TEST FOR EVEN:', num % 2);
+
+var app = {
+
+    isEven: function(input) {
+        if (input % 2 === 0) {
+            console.log('Your input of ' + input + ' is even');
+        } else {
+            console.log('Your input of ' + input + ' is odd');
+        }
+    }
+};
+
+app.isEven(2);
+app.isEven(3);
+app.isEven(4);
+app.isEven(5);
