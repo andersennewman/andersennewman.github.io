a8635aac60a8a3af521eb2b77b31b503f7e196dd
Andersen Newman <andersen.newman@bellevuecollege.edu>
2018-11-26 11:49:18 -0800

    week11Day2

---
+++ JavaScriptTest/radio-choices.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    const myForm = document.getElementById('sort-type');
+
+    myForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const value = document.querySelector('input[name="sorter"]:checked').value;
+        if (value.toLowerCase() === 'language') {
+            showChoice(value);
+        } else {
+            if (value.toLowerCase() === 'downloads') {
+                showChoice(value);
+            } else {
+                if (value.toLowerCase() === 'status') {
+                    showChoice(value);
+                } else {
+                    if (value.toLowerCase() === 'category') {
+                        showChoice(value);
+                    } else {
+                        showChoice(value);
+                    }
+                }
+            }
+        }
+    });
+}
+
+showChoice = (value) => {
+    const inputDis = document.getElementById('user-choice');
+
+    inputDis.textContent = value;
+}
